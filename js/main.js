71c2e5bc65c22c62628af71d3d12ef3d56ecc39a
ink-digital <100710018+ink-digital@users.noreply.github.com>
2022-04-18 17:57:14 -0400

    Final updates pending review

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ let cocktails = document.getElementById('cocktail_menu');
 let snack = document.getElementById('snack_menu');
 let wine = document.getElementById('wine_menu');
 let deepURL = "https://open.spotify.com/embed/playlist/27aXJ7nauChngKusXXxD97?utm_source=generator&theme=0";
+let monochromeURL = "https://open.spotify.com/embed/playlist/6KJw1k3RO61CGbX5OmycRs?utm_source=generator&theme=0";
+let pearlURL = "https://open.spotify.com/embed/playlist/3DiVzct2KxMvgxOpBVezLv?utm_source=generator&theme=0";
+let suiteURL = "https://open.spotify.com/embed/playlist/7yZhf78VtkvfUCsEy1AtZy?utm_source=generator&theme=0";
+let nightcapURL = "https://open.spotify.com/embed/playlist/36qGazBhzzODIFcMMHwFS5?utm_source=generator&theme=0";
+let glitterURL = "https://open.spotify.com/embed/playlist/5akFL82Wps7Qn23ai6uDkD?utm_source=generator&theme=0";
 let selectValue = document.getElementById('night-select').value;
 let musicSelect = document.getElementById('night-select');
 let spotifySrc = document.querySelector('.spotify').src;
@@ -18,38 +23,24 @@ Array.from(nightList).forEach((night) => {
   });
 });
 
-function showSpinner() {
-  spinner.classList.remove('d-none');
-  formSubmit.disabled = true;
-}
-
-// Old Code
-
-// panels.forEach((panel) => {
-//     panel.addEventListener('mouseover', () => {
-//         panel.classList.remove('col-2');
-//         panel.classList.add('isActive');s
-//         // panel.classList.add('active-panel');
-//     });
-
-//     panel.addEventListener('mouseout', () => {
-//             // removeActiveClasses();
-//             panel.classList.add('col-2');
-//             panel.classList.remove('isActive');
-//     });   
-// });
-
-// function removeActiveClasses() {
-//     panels.forEach((panel) => {
-//         panel.addEventListener('mouseout', () => {
-//             panel.classList.remove('active-panel');
-//         });
-//     });
+// function showSpinner() {
+//   spinner.classList.remove('d-none');
+//   formSubmit.disabled = true;
 // }
 
 function spotifyToggle(isSelected) {
-  if (isSelected == "Wednesday - Deep") {
+  if (isSelected == "Monday - Monochrome") {
+    document.querySelector('.spotify').src = monochromeURL;
+  } else if (isSelected == "Tuesday - Pearl") {
+    document.querySelector('.spotify').src = pearlURL;
+  } else if (isSelected == "Wednesday - Deep") {
     document.querySelector('.spotify').src = deepURL;
+  } else if (isSelected == "Thursday - Glitter") {
+    document.querySelector('.spotify').src = glitterURL;
+  } else if (isSelected == "Friday - Nightcap") {
+    document.querySelector('.spotify').src = nightcapURL;
+  } else if (isSelected == "Saturday - Suite") {
+    document.querySelector('.spotify').src = suiteURL;
   }
 }
 
@@ -68,19 +59,32 @@ panels.forEach(panel => panel.addEventListener('click', toggleOpen))
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
 
 function cocktailSwitch() {
-  document.getElementById("menu-image").src = "/images/menus/cocktails.jpg";
+  const drinkCarousel = document.getElementById("carouselDrinks");
+  const carousels = document.querySelectorAll('.carousel')
+  carousels.forEach(carousel => carousel.classList.add('d-none'));
+  drinkCarousel.classList.remove("d-none");
 }
 
 function snackSwitch() {
-  document.getElementById("menu-image").src = "/images/menus/snacks.jpg";
+  const snacksCarousel = document.getElementById("carouselSnacks");
+  const carousels = document.querySelectorAll('.carousel')
+  carousels.forEach(carousel => carousel.classList.add('d-none'));
+
+    snacksCarousel.classList.remove("d-none");
+
 }
 
 function wineSwitch() {
-  document.getElementById("menu-image").src = "/images/menus/wine_1.jpg";
+  const wineCarousel = document.getElementById("carouselWine");
+  const carousels = document.querySelectorAll('.carousel')
+  carousels.forEach(carousel => carousel.classList.add('d-none'));
+
+    wineCarousel.classList.remove("d-none");
+
 }
 
 cocktails.addEventListener("click", cocktailSwitch);
 snack.addEventListener("click", snackSwitch);
 wine.addEventListener("click", wineSwitch);
 musicSelect.addEventListener("select", spotifyToggle);
-formSubmit.addEventListener('click', showSpinner);
+// formSubmit.addEventListener('click', showSpinner);
